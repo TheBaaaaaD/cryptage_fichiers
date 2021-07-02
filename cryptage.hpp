f61b8109da5228d232593c8bd489b6c695ada0a6
TheBaaaaaD <62421223+TheBaaaaaD@users.noreply.github.com>
2021-07-02 12:52:41 +0200

    Create cryptage.hpp

---
+++ cryptage.hpp
@@ -0,0 +1,19 @@
+#ifndef CRYPTAGE_HPP
+#define CRYPTAGE_HPP
+
+#include <iostream>
+#include <string>
+#include <bitset>
+
+using namespace std;
+
+bitset<8> conversion(unsigned char byte)
+{
+    return bitset<8>(byte);
+}
+
+unsigned char crypter(unsigned char contenu, bitset<8> clef){
+    
+}
+
+#endif
